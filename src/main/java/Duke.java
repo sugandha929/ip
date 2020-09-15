84ff81002240770b3b5033809289cf5ab6e6d9b1
sugandha929 <sugandha929@gmail.com>
2020-09-15 18:51:30 +0530

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,14 +6,14 @@ import task.Task;
 import task.ToDos;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-
+    private static ArrayList<Task> listOfTasks = new ArrayList<>();
     public static void main (String[] args) throws commandException {
         String userInput;
         int taskIndex=0;
         Scanner in = new Scanner(System.in);
-        Task[] listOfTasks=new Task[100];
 
         System.out.println("Hello! I'm Groot");
         System.out.println("What can I do for you?");
@@ -27,32 +27,32 @@ public class Duke {
 
                     if (userInput.length() > 4 && userInput.contains("done")) {
                         int doneTaskNo = Integer.parseInt(userInput.substring(5)) - 1;
-                        listOfTasks[doneTaskNo].Done();
+                        listOfTasks.get(doneTaskNo).Done();
                         System.out.println("Nice! I've marked this task as done:\n" + "[\u2713] " +
-                                listOfTasks[doneTaskNo]);
+                                listOfTasks.get(doneTaskNo));
                     } else {
 
                         if (userInput.length() > 4 && userInput.contains("todo")) {
                             ToDos item = new ToDos(userInput.substring(5));
-                            listOfTasks[taskIndex] = item;
+                            listOfTasks.add(item);
                         } else if (userInput.length() > 8 && userInput.contains("deadline")) {
                             int slashIndex = userInput.indexOf("/");
                             Deadlines item = new Deadlines(userInput.substring(9, (slashIndex - 1)),
                                     userInput.substring(slashIndex + 4));
-                            listOfTasks[taskIndex] = item;
+                            listOfTasks.add(item);
                         } else if (userInput.length() > 5 && userInput.contains("event")) {
                             int slashIndex = userInput.indexOf("/");
                             Events item = new Events(userInput.substring(6, (slashIndex - 1)),
                                     userInput.substring(slashIndex + 4));
-                            listOfTasks[taskIndex] = item;
+                            listOfTasks.add(item);
                         }
                         else {
                             throw new commandException();
                         }
 
                         System.out.println("Got it. I've added this task: ");
-                        System.out.println(listOfTasks[taskIndex].getType() + listOfTasks[taskIndex].getMark() +
-                                " " + listOfTasks[taskIndex]);
+                        System.out.println(listOfTasks.get(taskIndex).getType() + listOfTasks.get(taskIndex).getMark() +
+                                " " + listOfTasks.get(taskIndex));
                         System.out.println("Now you have " + (taskIndex + 1) + " tasks in the list.");
                         taskIndex++;
                     }
@@ -60,8 +60,8 @@ public class Duke {
                     System.out.println("Here are the tasks in your list: ");
                     for (int x = 0; x < taskIndex; x++) {
                         int j = x + 1;
-                        System.out.println(j + "." + listOfTasks[x].getType() + listOfTasks[x].getMark() +
-                                " " + listOfTasks[x].toString());
+                        System.out.println(j + "." + listOfTasks.get(x).getType() + listOfTasks.get(x).getMark() +
+                                " " + listOfTasks.get(x).toString());
                     }
                 } else{
                     throw new commandException();
